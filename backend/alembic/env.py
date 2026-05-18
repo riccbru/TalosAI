df5511da909f0ed397c101f5dd10e06857839ef7
riccbru <74079434+riccbru@users.noreply.github.com>
2026-05-18 13:35:24 +0200

    Fixed Alembic usage

--- backend/alembic/env.py
+++ backend/alembic/env.py
@@ -4,9 +4,11 @@ from logging.config import fileConfig
 from sqlalchemy import pool
 from sqlalchemy.ext.asyncio import async_engine_from_config
 
+import app.models  # noqa: F401
 from alembic import context
 from app.core.config import settings
 from app.db.base import Base
+
 
 # this is the Alembic Config object, which provides
 # access to the values within the .ini file in use.
--- backend/app/db/base.py
+++ backend/app/db/base.py
@@ -1,3 +1,7 @@
+import importlib  # noqa: F401
+import os  # noqa: F401
+import pkgutil  # noqa: F401
+
 from sqlalchemy.orm import DeclarativeBase
 
 
